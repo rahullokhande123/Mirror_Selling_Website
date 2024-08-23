b0b3b4b2ad07124a1438a78d5f79133895240907
Rahul Lokhande <rlokhande06244@gmail.com>
2024-08-23 17:45:56 +0530

    Mirror_Selling_Website

---
+++ script.js
@@ -0,0 +1,57 @@
+function ram1(){
+    document.getElementById("chan1").src = "https://niches.minimog.co/cdn/shop/products/16-b_8ea7ef4c-a9b8-48ab-9eef-2877cbf08364.jpg?v=1623746487&width=750";
+}
+function sita1(){
+    document.getElementById("chan1").src = "https://niches.minimog.co/cdn/shop/products/16-a_bb691acd-e42f-4505-8332-3ba229608762.jpg?v=1623746487&width=720";
+}
+// =============================================================================
+
+function ram2(){
+    document.getElementById("chan2").src = "https://niches.minimog.co/cdn/shop/products/5-b_649c0c1d-d74c-4076-8832-de8bcee2ca11.jpg?v=1623746109&width=535";
+}
+function sita2(){
+    document.getElementById("chan2").src = "https://niches.minimog.co/cdn/shop/products/5-a_ac7a68a2-8f08-42ba-b8a4-a9f2d7bc1426.jpg?v=1623746109&width=360";
+}
+// ==============================================================================
+function ram3(){
+    document.getElementById("chan3").src = "https://niches.minimog.co/cdn/shop/products/12-b_5d417d50-5b80-4860-9c59-a21c03b8c205.jpg?v=1623746364&width=535";
+}
+function sita3(){
+    document.getElementById("chan3").src = "https://niches.minimog.co/cdn/shop/products/12-a_33430a3e-1b90-45d4-b153-974ef95d0638.jpg?v=1623746363&width=360";
+}
+// ==============================================================================
+function ram4(){
+    document.getElementById("chan4").src = "https://niches.minimog.co/cdn/shop/products/19-b_2e623f37-eda2-4d1f-bbde-6b1f1c04e44a.jpg?v=1623746556&width=535";
+}
+function sita4(){
+    document.getElementById("chan4").src = "https://niches.minimog.co/cdn/shop/products/19-a_e38c1099-b599-4172-b2df-617ba75444ff.jpg?v=1623746556&width=360";
+}
+
+//=============================== imgHead2 Section ============================= 
+function ram5(){
+    document.getElementById("chan5").src = "https://niches.minimog.co/cdn/shop/products/18-b_73e0d8ad-fc2c-4ed3-b13b-95ee7d77be16.jpg?v=1623746535&width=535";
+}
+function sita5(){
+    document.getElementById("chan5").src = "https://niches.minimog.co/cdn/shop/products/18-a_1f02a0e4-14b6-4445-bc02-f18ee82e7fd9.jpg?v=1623746535&width=360";
+}
+// =============================================================================
+function ram6(){
+    document.getElementById("chan6").src = "https://niches.minimog.co/cdn/shop/products/2-b_0c455f92-3775-4f22-8fd5-ad6732124cea.jpg?v=1623745793&width=535";
+}
+function sita6(){
+    document.getElementById("chan6").src = "https://niches.minimog.co/cdn/shop/products/2-a_e0cd8381-5c93-4175-aed7-df9b38e92d2b.jpg?v=1623745793&width=360";
+}
+// =============================================================================
+function ram7(){
+    document.getElementById("chan7").src = "https://niches.minimog.co/cdn/shop/products/20-b_975f572d-e1e0-4dc1-8b18-1bd0a58afb0f.jpg?v=1623746577&width=535";
+}
+function sita7(){
+    document.getElementById("chan7").src = "https://niches.minimog.co/cdn/shop/products/20-a_4feca483-2971-433d-9981-2290a851d194.jpg?v=1623746577&width=535";
+}
+// =============================================================================
+function ram8(){
+    document.getElementById("chan8").src = "https://niches.minimog.co/cdn/shop/products/4-b_0af0b9c6-10f3-4ef3-a8c0-4e96df8fab0a.jpg?v=1623746080&width=535";
+}
+function sita8(){
+    document.getElementById("chan8").src = "https://niches.minimog.co/cdn/shop/products/4-a_56c6af01-31fc-49f4-8ca4-827cf0f8b082.jpg?v=1623746079&width=535";
+}
